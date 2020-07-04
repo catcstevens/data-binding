6cd92c8bc4ce6aa2beaffe92fe1c0ec228b2a01d
Cat <ccallcottstevens@gmail.com>
2020-07-05 07:28:56 +1000

    changed font size and color to blue

--- src/components/User.js
+++ src/components/User.js
@@ -1,5 +1,10 @@
 import React from 'react';
 
+const userStyles = {
+    color: 'blue',
+    fontSize: '25px'
+}
+
 const Users = props => {
     function onChangeName(event) {
         props.changeEvent(props.id, event.target.value)
@@ -10,9 +15,9 @@ const Users = props => {
     //user function returns all the User specific UI components
     return (
         <div>
-            <h3>I am {props.name}</h3>
-            <input type='text' onChange={onChangeName} value={props.name} />
-            <button onClick={onDeleteName}>Delete</button>
+            <h3 style={userStyles}> I am {props.name}</h3>
+            <input syle={userStyles} type='text' onChange={onChangeName} value={props.name} />
+            <button style={userStyles} onClick={onDeleteName}>Delete</button>
         </div>
     )
 }
